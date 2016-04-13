75d8619533e5eb16136f801b2dc4ca0eee15b321
Malcolm J Sackler <mal@Malcolm-Sacklers-Computer.local>
2016-04-13 14:40:43 -0400

    axios test

--- frontend/app/components/Home.js
+++ frontend/app/components/Home.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import {Link} from 'react-router'
+import {Link} from 'react-router';
+import AjaxHelper from '../utils/AjaxHelpers'
 
 const Home = React.createClass({
+  clicked:function(){
+    console.log('clicked');
+    AjaxHelper.findMovie()
+    .then(function(response){
+      console.log(response.data);
+      this.setState({
+        ajaxReturn: response.data
+      });
+    }.bind(this))
+    .catch(function(err){
+      console.warn('err');
+      return err;
+    })
+  },
   render: function(){
     return(
       <div>
         <h3>Home</h3>
+        <button onClick={this.clicked}>submit</button>
       </div>
     )
   }
--- frontend/app/utils/AjaxHelpers.js
+++ frontend/app/utils/AjaxHelpers.js
@@ -2,6 +2,12 @@ import axios from 'axios';
 
 const AjaxHelpers = {
 
-  
+  findMovie: function(name) {
+    return axios.get('https://api-public.guidebox.com/v1.43/US/b5pWO2d8FjWYHZ7gWXPmUQJGZa2oJK/search/movie/title/TheMatrix/exact');
+
+  },
+
+
 
 }
+export default AjaxHelpers;
