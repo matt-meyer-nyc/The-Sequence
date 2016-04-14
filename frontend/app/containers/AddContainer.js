56608a2831fe780f4da293d9e3825b74c58001c8
Malcolm J Sackler <mal@Malcolm-Sacklers-Computer.local>
2016-04-14 11:54:29 -0400

    added author and can now save data in object

--- frontend/app/containers/AddContainer.js
+++ frontend/app/containers/AddContainer.js
@@ -18,6 +18,11 @@ const AddContainer = React.createClass({
     };
   },
 
+  onChangeAuthor: function(e) {
+    this.setState({
+      Author: e.target.value
+    })
+  },
   onChangeName: function(e) {
     this.setState({
       Name: e.target.value
@@ -52,22 +57,39 @@ const AddContainer = React.createClass({
       Movie3: this.state.Movie3,
       Description: this.state.Description
     }
-    var movieArray = [this.state.Movie1, this.state.Movie2, this.state.Movie3];
+  var AddPlaylist = {name:'', playlists: [ { listname:'', movies: [] }] }
+  var movieArray = [this.state.Movie1, this.state.Movie2, this.state.Movie3];
+
+
 console.log(this.state.Name);
 for(var i = 0; i < movieArray.length; i++){
 
     AjaxHelper.getMovies(movieArray[i])
     .then(function(response){
-      console.log(response.data.results[0].title);
+      console.log(response.data.results[0]);
+      console.log(response.data.results[0].release_year);
+
+      var movieList = {
+        title: response.data.results[0].title,
+        poster: response.data.results[0].poster_240x342,
+        year: response.data.results[0].release_year
+      }
+      AddPlaylist.playlists[0].movies.push(movieList);
+      console.log('playlist',AddPlaylist.playlists[0].movies);
+
       this.setState({
         listAdded: "added"
       });
+
     }.bind(this))
     .catch(function(err){
-      console.warn('err');
+      console.warn('err', err);
       return err;
     })
   }
+
+
+
 },
 
 
@@ -76,7 +98,8 @@ for(var i = 0; i < movieArray.length; i++){
   render: function(){
     return(
       <div className="display">
-        <Add onChangeName={this.onChangeName}
+        <Add onChangeAuthor={this.onChangeAuthor}
+          onChangeName={this.onChangeName}
           onChangeMovie1={this.onChangeMovie1}
           onChangeMovie2={this.onChangeMovie2}
           onChangeMovie3={this.onChangeMovie3}
