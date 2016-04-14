ab23a275456be305fcc11a3cabc60b9098861964
JulioMontas <julio86@gmail.com>
2016-04-14 19:34:37 -0400

    logo style

--- frontend/app/components/Header.js
+++ frontend/app/components/Header.js
@@ -6,7 +6,7 @@ function Header(props){
       <nav>
         <ul>
           <li><a href="/#/add">+</a></li>
-          <li><h1><a href="/#/">The Sequence</a></h1></li>
+          <li><h1><a href="/#/"><span>The</span>Sequence</a></h1></li>
           <li><a href="/#/search">Search</a></li>
         </ul>
       </nav>
