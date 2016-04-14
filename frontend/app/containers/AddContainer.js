c6f5b03bc5f9de6175ff9cd51a3808ea32d2aafa
Malcolm J Sackler <mal@Malcolm-Sacklers-Computer.local>
2016-04-14 17:33:09 -0400

    cleaned up addcontainer

--- frontend/app/containers/AddContainer.js
+++ frontend/app/containers/AddContainer.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import axios from 'axios';
-
 import Add from '../components/Add';
 import AjaxHelper from '../utils/AjaxHelpers';
-
 
 const AddContainer = React.createClass({
   getInitialState: function() {
     return {
-
       Name: '',
       Title: '',
       Movie1: '',
@@ -59,57 +56,42 @@ const AddContainer = React.createClass({
       Movie3: this.state.Movie3,
       Description: this.state.Description
     }
-  var AddPlaylist = {author:'', playlists: [ { title:'', movies: [] }] }
-  var movieArray = [this.state.Movie1, this.state.Movie2, this.state.Movie3];
+    var AddPlaylist = {author:'', playlists: [ { title:'', movies: [] }] }
+    var movieArray = [this.state.Movie1, this.state.Movie2, this.state.Movie3];
 
-AddPlaylist.author = (List.Author);
-AddPlaylist.playlists[0].title = (List.Title);
-console.log(List.Movie1);
+    AddPlaylist.author = (List.Author);
+    AddPlaylist.playlists[0].title = (List.Title);
 
-console.log(List.Title);
-for(var i = 0; i < movieArray.length; i++){
-
-    AjaxHelper.getMovies(movieArray[i])
-    .then(function(response){
-      console.log(response.data.results[0]);
-      console.log(response.data.results[0].release_year);
-
-      var movieList = {
-        title: response.data.results[0].title,
-        poster: response.data.results[0].poster_240x342,
-        year: response.data.results[0].release_year
-      }
-      AddPlaylist.playlists[0].movies.push(movieList);
+    for(var i = 0; i < movieArray.length; i++){
+        AjaxHelper.getMovies(movieArray[i])
+        .then(function(response){
+          var movieList = {
+            title: response.data.results[0].title,
+            poster: response.data.results[0].poster_240x342,
+            year: response.data.results[0].release_year
+          }
+          AddPlaylist.playlists[0].movies.push(movieList);
+        }.bind(this))
+        .catch(function(err){
+          console.warn('err', err);
+          return err;
+        });
+      };
+      setTimeout(function(){
+        AjaxHelper.addMovies(AddPlaylist)
+        .then(function(response){
+          console.log(response.data);
+          this.setState({
+            listAdded: "added"
+          });
+        }.bind(this))
+        .catch(function(err){
+          console.warn('err', err);
+          return err;
+        })
+      }, 1000);
       console.log('playlist',AddPlaylist);
-
-    }.bind(this))
-    .catch(function(err){
-      console.warn('err', err);
-      return err;
-    });
-  };
-  //needs work
-setTimeout(function(){
-  AjaxHelper.addMovies(AddPlaylist)
-  .then(function(response){
-    console.log(response.data);
-  }.bind(this))
-
-  .catch(function(err){
-
-    console.warn('err', err);
-    return err;
-
-  })
-  }, 3000);
-
-
-
-},
-
-
-
-
+  },
   render: function(){
     return(
       <div className="display">
