ec9fd248d0a30a5ba4101424d47d4707832c68fe
JulioMontas <julio86@gmail.com>
2016-04-15 14:02:48 -0400

    making changes for the list

--- frontend/app/components/Home.js
+++ frontend/app/components/Home.js
@@ -4,20 +4,6 @@ import AjaxHelper from '../utils/AjaxHelpers'
 
 
 const Home = React.createClass({
-  clicked:function(){
-    console.log('clicked');
-    AjaxHelper.findMovie()
-    .then(function(response){
-      console.log(response.data);
-      this.setState({
-        ajaxReturn: response.data
-      });
-    }.bind(this))
-    .catch(function(err){
-      console.warn('err');
-      return err;
-    })
-  },
   render: function(){
     return(
       <div>
--- frontend/app/components/List.js
+++ frontend/app/components/List.js
@@ -5,9 +5,13 @@ function List(props){
   console.log(props.movies[0]);
   const MovieList = props.movies.map((movie) =>{
     return <li key={movie._id}>
+
+
       <b>Title:</b> {movie.playlists[0].title}<br/>
       <b>Author:</b> {movie.author}<br/>
       <img src={movie.playlists[0].movies[0].poster} onClick={console.log('clicked')}/>
+
+
     </li>;
   });
   return (
