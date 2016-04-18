2d49b4a3036d0b9207bf4fd0bacd7e5504897987
JulioMontas <julio86@gmail.com>
2016-04-18 09:33:25 -0400

    index style change and api

--- frontend/app/utils/AjaxHelpers.js
+++ frontend/app/utils/AjaxHelpers.js
@@ -3,10 +3,10 @@ import axios from 'axios';
 const AjaxHelpers = {
 
   findMovie: function() {
-    return axios.get('https://api-public.guidebox.com/v1.43/US/ooiQChiJ840yDpftEosaGioiDJQ0Yb/search/movie/title/+'+ movieArray +'/exact')
+    return axios.get('https://api-public.guidebox.com/v1.43/US/rKhuJkXJ8M9fIKSJDUm2eqsz2QiOVR8A/search/movie/title/+'+ movieArray +'/exact')
   },
   getMovies: function(movieArray){
-    return axios.get('https://api-public.guidebox.com/v1.43/US/ooiQChiJ840yDpftEosaGioiDJQ0Yb/search/movie/title/+'+ movieArray +'/exact')
+    return axios.get('https://api-public.guidebox.com/v1.43/US/rKhuJkXJ8M9fIKSJDUm2eqsz2QiOVR8A/search/movie/title/+'+ movieArray +'/exact')
   },
   showMovies: function(){
     return axios.get('http://localhost:3000/movies');
